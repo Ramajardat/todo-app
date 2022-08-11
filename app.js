9b55f85744fed31d20ad81ff177f97f009c0c4ff
Ramajardat <ramajardat6@gmail.com>
2022-08-11 16:36:24 -0700

    Added new branch

--- app.js
+++ app.js
@@ -18,3 +18,60 @@ if (welcomeMsg) {
         alert("welcome to our Stars Fandom  " + userName);
 }
 
+
+const array_questions = [];
+
+let like = prompt("Do you like Orion?");
+array_questions[0] = like
+
+let star = prompt("Do you like star?");
+array_questions[1] = star
+
+let join = prompt("Do you want to join our club?");
+array_questions[2] = join
+
+
+
+
+for(let i = 0; i < array_questions.length; i++)
+{
+    switch(array_questions[i])
+    {
+        case "yes" :
+        array_questions[i] = "yes"
+        break;
+
+        case "no" :
+        array_questions[i] = "no"
+        break;
+    
+    
+        case "y" :
+        array_questions[i] = "y"
+        break;
+    
+        case "n" :
+        array_questions[i] = "n"
+        break;
+    
+        default : 
+        array_questions[i] = "Invalid"
+        break;
+    
+    }
+
+    console.log(array_questions[i]);
+
+}
+
+
+let stars = prompt("please can you  rate our club ?")
+if (rate < 0)
+    rate = 0;
+else if (rate > 5)
+    rate = 5;
+
+
+    alert("thanks for your rate"+ stars);
+
+    console.log(stars);
